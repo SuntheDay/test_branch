f38d2bdbce21e9d917f43c32b9af897158d49394
SuntheDay <2376847176@qq.com>
2019-10-20 19:41:23 +0800

    del

--- code/staticlib/mylib.h
+++
@@ -1,5 +0,0 @@
-int max(int *,int);
-void show(int *,int);
-int sum(int *,int);
-int init(int *,int);
-void urand(int *,int);
--- linuxprogram/week6/code/dynamiclib/dylib.h
+++
@@ -1,4 +0,0 @@
-int max(int *,int);
-void show(int *,int);
-int sum(int *,int);
-void init(int *,int);
